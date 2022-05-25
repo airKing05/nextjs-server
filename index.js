f47bdd49eaaf2b61f0e5ee88391cf38b0fe014f0
airKing05 <anil050797@gmail.com>
2022-05-25 19:50:46 +0530

    cors added

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const dotenv = require('dotenv');
 const express = require('express');
 const path = require('path');
+const cors = require('cors')
 
 const app = express();
+
+// connecting to backend
+app.use(cors());
 
 // console.log(path)
 
